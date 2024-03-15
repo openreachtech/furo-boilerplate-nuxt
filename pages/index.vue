95b89a1e0272398ccae519ed29a875bae0138623
Stew Eucen <stew.eucen@gmail.com>
2024-03-15 18:55:32 +0900

    Build index page

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<!-- pages/index.vue -->
+<template>
+  <h1>Hello I&#39;m pages/index.vue!</h1>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <!-- <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div> -->
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+/* CSS styles here */
+</style>
