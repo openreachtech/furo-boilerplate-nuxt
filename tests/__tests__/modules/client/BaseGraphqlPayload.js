a0f565122abf502f75d562d088e066ce8e14cd5c
Stew Eucen <stew.eucen@gmail.com>
2024-03-18 13:32:32 +0900

    Add test for BaseGraphqlPayload#generateQuery()

--- tests/__tests__/modules/client/BaseGraphqlPayload.js
+++ tests/__tests__/modules/client/BaseGraphqlPayload.js
@@ -258,3 +258,320 @@ describe('BaseGraphqlPayload', () => {
     })
   })
 })
+
+describe('BaseGraphqlPayload', () => {
+  describe('#generateQuery()', () => {
+    describe('query with no input', () => {
+      const cases = [
+        {
+          params: {
+            queryTemplate: `
+              query pickUpForumTopics {
+                pickUpForumTopics {
+                  pickUpForumTopics {
+                    id
+                    forumCategory {
+                      id
+                      name
+                    }
+                    name
+                    descriptionHtml
+                    proposer {
+                      customerId
+                      username
+                      avatarUrl
+                      customerRoles {
+                        id
+                        name
+                      }
+                    }
+                    proposedAt
+                    editedAt
+                    totalForumPost
+                    latestForumPostPostedAt
+                  }
+                }
+              }
+            `,
+          },
+        },
+        {
+          params: {
+            queryTemplate: `
+              query {
+                curriculums {
+                  curriculums {
+                    id
+                    title
+                    description
+                    thumbnailUrl
+                    postedAt
+                  }
+                  pagination {
+                    limit
+                    offset
+                    sort {
+                      targetColumn
+                      orderBy
+                    }
+                    totalRecords
+                  }
+                }
+              }
+            `,
+          },
+        },
+      ]
+
+      describe.each(cases)('queryTemplate: $params.queryTemplate', ({ params }) => {
+        const args = [
+          {
+            input: {},
+          },
+          {
+            input: {},
+          },
+        ]
+
+        test.each(args)('input: $input', ({ input }) => {
+          const expected = params.queryTemplate
+
+          const querySpy = jest.spyOn(BaseGraphqlPayload, 'query', 'get')
+            .mockReturnValue(params.queryTemplate)
+
+          const payload = BaseGraphqlPayload.create()
+
+          const actual = payload.generateQuery({
+            input,
+          })
+
+          expect(actual)
+            .toBe(expected)
+
+          querySpy.mockRestore()
+        })
+
+        test('with no input', () => {
+          const expected = params.queryTemplate
+
+          const querySpy = jest.spyOn(BaseGraphqlPayload, 'query', 'get')
+            .mockReturnValue(params.queryTemplate)
+
+          const payload = BaseGraphqlPayload.create()
+
+          const actual = payload.generateQuery()
+
+          expect(actual)
+            .toBe(expected)
+
+          querySpy.mockRestore()
+        })
+      })
+    })
+
+    describe('query with input', () => {
+      const cases = [
+        {
+          params: {
+            queryTemplate: `
+                query pickUpForumTopics (input: $input) {
+                  pickUpForumTopics {
+                    pickUpForumTopics {
+                      id
+                      forumCategory {
+                        id
+                        name
+                      }
+                      name
+                      descriptionHtml
+                      proposer {
+                        customerId
+                        username
+                        avatarUrl
+                        customerRoles {
+                          id
+                          name
+                        }
+                      }
+                      proposedAt
+                      editedAt
+                      totalForumPost
+                      latestForumPostPostedAt
+                    }
+                  }
+                }
+            `,
+          },
+          inputCases: [
+            {
+              input: {
+                pickUpForumTopicId: 10001,
+              },
+              expected: `
+                query pickUpForumTopics (input: {"pickUpForumTopicId":10001}) {
+                  pickUpForumTopics {
+                    pickUpForumTopics {
+                      id
+                      forumCategory {
+                        id
+                        name
+                      }
+                      name
+                      descriptionHtml
+                      proposer {
+                        customerId
+                        username
+                        avatarUrl
+                        customerRoles {
+                          id
+                          name
+                        }
+                      }
+                      proposedAt
+                      editedAt
+                      totalForumPost
+                      latestForumPostPostedAt
+                    }
+                  }
+                }
+            `,
+            },
+            {
+              input: {
+                pickUpForumTopicId: 10002,
+              },
+              expected: `
+                query pickUpForumTopics (input: {"pickUpForumTopicId":10002}) {
+                  pickUpForumTopics {
+                    pickUpForumTopics {
+                      id
+                      forumCategory {
+                        id
+                        name
+                      }
+                      name
+                      descriptionHtml
+                      proposer {
+                        customerId
+                        username
+                        avatarUrl
+                        customerRoles {
+                          id
+                          name
+                        }
+                      }
+                      proposedAt
+                      editedAt
+                      totalForumPost
+                      latestForumPostPostedAt
+                    }
+                  }
+                }
+            `,
+            },
+          ],
+        },
+        {
+          params: {
+            queryTemplate: `
+                query {
+                  curriculums (input: $input) {
+                    curriculums {
+                      id
+                      title
+                      description
+                      thumbnailUrl
+                      postedAt
+                    }
+                    pagination {
+                      limit
+                      offset
+                      sort {
+                        targetColumn
+                        orderBy
+                      }
+                      totalRecords
+                    }
+                  }
+                }
+            `,
+          },
+          inputCases: [
+            {
+              input: {
+                curriculumId: 20001,
+              },
+              expected: `
+                query {
+                  curriculums (input: {"curriculumId":20001}) {
+                    curriculums {
+                      id
+                      title
+                      description
+                      thumbnailUrl
+                      postedAt
+                    }
+                    pagination {
+                      limit
+                      offset
+                      sort {
+                        targetColumn
+                        orderBy
+                      }
+                      totalRecords
+                    }
+                  }
+                }
+            `,
+            },
+            {
+              input: {
+                curriculumId: 20002,
+              },
+              expected: `
+                query {
+                  curriculums (input: {"curriculumId":20002}) {
+                    curriculums {
+                      id
+                      title
+                      description
+                      thumbnailUrl
+                      postedAt
+                    }
+                    pagination {
+                      limit
+                      offset
+                      sort {
+                        targetColumn
+                        orderBy
+                      }
+                      totalRecords
+                    }
+                  }
+                }
+            `,
+            },
+          ],
+        },
+      ]
+
+      describe.each(cases)('queryTemplate: $params.queryTemplate', ({ params, inputCases }) => {
+        test.each(inputCases)('input: $input', ({ input, expected }) => {
+          const querySpy = jest.spyOn(BaseGraphqlPayload, 'query', 'get')
+            .mockReturnValue(params.queryTemplate)
+
+          const payload = BaseGraphqlPayload.create()
+
+          const actual = payload.generateQuery({
+            input,
+          })
+
+          expect(actual)
+            .toBe(expected)
+
+          querySpy.mockRestore()
+        })
+      })
+    })
+  })
+})
