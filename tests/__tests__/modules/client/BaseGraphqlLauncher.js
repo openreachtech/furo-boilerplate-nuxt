b05f4cb855194761571d94166b237d963c2b7791
Stew Eucen <stew.eucen@gmail.com>
2024-03-20 18:33:26 +0900

    Add test for BaseGraphqlLauncher as stub

---
+++ tests/__tests__/modules/client/BaseGraphqlLauncher.js
@@ -0,0 +1,96 @@
+import {
+  ConstructorSpyGenerator,
+} from '@openreachtech/renchan-test-tools'
+
+import BaseGraphqlLauncher from '@/modules/client/BaseGraphqlLauncher'
+
+describe('BaseGraphqlLauncher', () => {
+  describe('constructor', () => {
+    describe('to keep property', () => {
+      describe('#config', () => {
+        const cases = [
+          {
+            params: {
+              config: {
+                ENDPOINT_URL: 'http://example.com/graphql-customer',
+              },
+            },
+          },
+          {
+            params: {
+              config: {
+                ENDPOINT_URL: 'http://example.com/graphql-admin',
+              },
+            },
+          },
+        ]
+
+        test.each(cases)('ENDPOINT_URL: $params.config.ENDPOINT_URL', ({ params }) => {
+          const launcher = new BaseGraphqlLauncher(params)
+
+          expect(launcher)
+            .toHaveProperty('config', params.config)
+        })
+      })
+    })
+  })
+})
+
+describe('BaseGraphqlLauncher', () => {
+  describe('.create()', () => {
+    describe('to be instance of own class', () => {
+      const cases = [
+        {
+          params: {
+            config: {
+              ENDPOINT_URL: 'http://example.com/graphql-customer',
+            },
+          },
+        },
+        {
+          params: {
+            config: {
+              ENDPOINT_URL: 'http://example.com/graphql-admin',
+            },
+          },
+        },
+      ]
+
+      test.each(cases)('ENDPOINT_URL: $params.config.ENDPOINT_URL', ({ params }) => {
+        const launcher = BaseGraphqlLauncher.create(params)
+
+        expect(launcher)
+          .toBeInstanceOf(BaseGraphqlLauncher)
+      })
+    })
+
+    describe('to call constructor', () => {
+      const cases = [
+        {
+          params: {
+            config: {
+              ENDPOINT_URL: 'http://example.com/graphql-customer',
+            },
+          },
+        },
+        {
+          params: {
+            config: {
+              ENDPOINT_URL: 'http://example.com/graphql-admin',
+            },
+          },
+        },
+      ]
+
+      test.each(cases)('ENDPOINT_URL: $params.config.ENDPOINT_URL', ({ params }) => {
+        const DerivedClass = ConstructorSpyGenerator.create({ jest })
+          .generateSpyKitClass(BaseGraphqlLauncher)
+
+        DerivedClass.create(params)
+
+        expect(DerivedClass.__spy__)
+          .toHaveBeenCalledWith(params)
+      })
+    })
+  })
+})
