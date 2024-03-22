8a6ea299f8b9344f67d2657f1f0c447f5943b1a1
Stew Eucen <stew.eucen@gmail.com>
2024-03-23 08:47:40 +0900

    Add test for CurriculumsGraphqlLauncher.get:Capsule

--- tests/__tests__/app/graphql/client/Curriculums/CurriculumsGraphqlLauncher.js
+++ tests/__tests__/app/graphql/client/Curriculums/CurriculumsGraphqlLauncher.js
@@ -1,6 +1,7 @@
 import CurriculumsGraphqlLauncher from '@/app/graphql/client/Curriculums/CurriculumsGraphqlLauncher'
 import BaseAppGraphqlLauncher from '~/app/graphql/client/BaseAppGraphqlLauncher'
 import CurriculumsGraphqlPayload from '~/app/graphql/client/Curriculums/CurriculumsGraphqlPayload'
+import CurriculumsGraphqlCapsule from '~/app/graphql/client/Curriculums/CurriculumsGraphqlCapsule'
 
 describe('CurriculumsGraphqlLauncher', () => {
   describe('super class', () => {
@@ -20,6 +21,17 @@ describe('CurriculumsGraphqlLauncher', () => {
 
       expect(actual)
         .toBe(CurriculumsGraphqlPayload) // same reference
+    })
+  })
+})
+
+describe('CurriculumsGraphqlLauncher', () => {
+  describe('.get:Capsule', () => {
+    test('to be CurriculumsGraphqlCapsule', () => {
+      const actual = CurriculumsGraphqlLauncher.Capsule
+
+      expect(actual)
+        .toBe(CurriculumsGraphqlCapsule) // same reference
     })
   })
 })
