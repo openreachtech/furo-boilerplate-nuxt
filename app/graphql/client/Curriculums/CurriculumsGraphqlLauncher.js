baa6b4b192884a9298ea330fe7b941b477b3058d
Stew Eucen <stew.eucen@gmail.com>
2024-03-23 08:48:19 +0900

    Define CurriculumsGraphqlLauncher.get:Payload

--- app/graphql/client/Curriculums/CurriculumsGraphqlLauncher.js
+++ app/graphql/client/Curriculums/CurriculumsGraphqlLauncher.js
@@ -1,5 +1,9 @@
 import BaseAppGraphqlLauncher from '~/app/graphql/client/BaseAppGraphqlLauncher'
+import CurriculumsGraphqlPayload from '~/app/graphql/client/Curriculums/CurriculumsGraphqlPayload'
 
 export default class CurriculumsGraphqlLauncher extends BaseAppGraphqlLauncher {
-
+  /** @inheritdoc */
+  static get Payload () {
+    return CurriculumsGraphqlPayload
+  }
 }
