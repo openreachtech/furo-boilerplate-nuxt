e1beeac02983a2156374cc6cd303af94bf090841
Stew Eucen <stew.eucen@gmail.com>
2024-03-23 08:47:25 +0900

    Add test for CurriculumsGraphqlLauncher.get:Payload

--- tests/__tests__/app/graphql/client/Curriculums/CurriculumsGraphqlLauncher.js
+++ tests/__tests__/app/graphql/client/Curriculums/CurriculumsGraphqlLauncher.js
@@ -1,5 +1,6 @@
 import CurriculumsGraphqlLauncher from '@/app/graphql/client/Curriculums/CurriculumsGraphqlLauncher'
 import BaseAppGraphqlLauncher from '~/app/graphql/client/BaseAppGraphqlLauncher'
+import CurriculumsGraphqlPayload from '~/app/graphql/client/Curriculums/CurriculumsGraphqlPayload'
 
 describe('CurriculumsGraphqlLauncher', () => {
   describe('super class', () => {
@@ -8,6 +9,17 @@ describe('CurriculumsGraphqlLauncher', () => {
 
       expect(actual)
         .toBeInstanceOf(BaseAppGraphqlLauncher)
+    })
+  })
+})
+
+describe('CurriculumsGraphqlLauncher', () => {
+  describe('.get:Payload', () => {
+    test('to be CurriculumsGraphqlPayload', () => {
+      const actual = CurriculumsGraphqlLauncher.Payload
+
+      expect(actual)
+        .toBe(CurriculumsGraphqlPayload) // same reference
     })
   })
 })
