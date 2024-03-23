c927c72f4afdfb13f93c0c15ca9821217c12facc
Stew Eucen <stew.eucen@gmail.com>
2024-03-24 01:44:19 +0900

    Build curriculums page <template>

---
+++ pages/curriculums.vue
@@ -0,0 +1,17 @@
+<!-- pages/index.vue -->
+<template>
+  <h1>Hello I&#39;m pages/curriculums.vue!</h1>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+/* CSS styles here */
+</style>
