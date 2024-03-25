d34adb3538e9a57e6ae686514a0bacd0511348d5
Stew Eucen <stew.eucen@gmail.com>
2024-03-25 22:10:55 +0900

    Add test for GenericGraphqlPayload.create()

--- tests/__tests__/modules/client/payloads/GenericGraphqlPayload.js
+++ tests/__tests__/modules/client/payloads/GenericGraphqlPayload.js
@@ -1,3 +1,7 @@
+import {
+  ConstructorSpyGenerator,
+} from '@openreachtech/renchan-test-tools'
+
 import GenericGraphqlPayload from '@/modules/client/payloads/GenericGraphqlPayload'
 import BaseGraphqlPayload from '~/modules/client/BaseGraphqlPayload'
 
@@ -8,6 +12,178 @@ describe('GenericGraphqlPayload', () => {
 
       expect(actual)
         .toBeInstanceOf(BaseGraphqlPayload)
+    })
+  })
+})
+
+describe('GenericGraphqlPayload', () => {
+  describe('.create', () => {
+    describe('to return instance of this class', () => {
+      const cases = [
+        {
+          params: {
+            queryTemplate: `
+              query pickUpForumTopics {
+                pickUpForumTopics {
+                  pickUpForumTopics {
+                    id
+                    forumCategory {
+                      id
+                      name
+                    }
+                    name
+                    descriptionHtml
+                    proposer {
+                      customerId
+                      username
+                      avatarUrl
+                      customerRoles {
+                        id
+                        name
+                      }
+                    }
+                    proposedAt
+                    editedAt
+                    totalForumPost
+                    latestForumPostPostedAt
+                  }
+                }
+              }
+            `,
+            input: null,
+            options: {
+              mode: 'cors',
+            },
+          },
+        },
+        {
+          params: {
+            queryTemplate: `
+              query {
+                curriculums(input: $input) {
+                  curriculums {
+                    id
+                    title
+                    description
+                    thumbnailUrl
+                    postedAt
+                  }
+                  pagination {
+                    limit
+                    offset
+                    sort {
+                      targetColumn
+                      orderBy
+                    }
+                    totalRecords
+                  }
+                }
+              }
+            `,
+            input: {
+              id: 10001,
+            },
+            options: {
+              credentials: 'omit',
+            },
+          },
+        },
+      ]
+
+      test.each(cases)('input: $params.input', ({ params }) => {
+        const actual = GenericGraphqlPayload.create(params)
+
+        expect(actual)
+          .toBeInstanceOf(GenericGraphqlPayload)
+      })
+    })
+
+    describe('to call constructor', () => {
+      const cases = [
+        {
+          params: {
+            queryTemplate: `
+              query pickUpForumTopics {
+                pickUpForumTopics {
+                  pickUpForumTopics {
+                    id
+                    forumCategory {
+                      id
+                      name
+                    }
+                    name
+                    descriptionHtml
+                    proposer {
+                      customerId
+                      username
+                      avatarUrl
+                      customerRoles {
+                        id
+                        name
+                      }
+                    }
+                    proposedAt
+                    editedAt
+                    totalForumPost
+                    latestForumPostPostedAt
+                  }
+                }
+              }
+            `,
+            input: null,
+            options: {
+              mode: 'cors',
+            },
+          },
+        },
+        {
+          params: {
+            queryTemplate: `
+              query {
+                curriculums(input: $input) {
+                  curriculums {
+                    id
+                    title
+                    description
+                    thumbnailUrl
+                    postedAt
+                  }
+                  pagination {
+                    limit
+                    offset
+                    sort {
+                      targetColumn
+                      orderBy
+                    }
+                    totalRecords
+                  }
+                }
+              }
+            `,
+            input: {
+              id: 10001,
+            },
+            options: {
+              credentials: 'omit',
+            },
+          },
+        },
+      ]
+
+      test.each(cases)('input: $params.input', ({ params }) => {
+        const querySpy = jest.spyOn(GenericGraphqlPayload, 'query', 'get')
+          .mockReturnValue(params.queryTemplate)
+
+        const DerivedClass = ConstructorSpyGenerator.create({ jest })
+          .generateSpyKitClass(GenericGraphqlPayload)
+
+        DerivedClass.create(params)
+
+        expect(DerivedClass.__spy__)
+          .toHaveBeenCalledWith(params)
+
+        querySpy.mockRestore()
+      })
     })
   })
 })
