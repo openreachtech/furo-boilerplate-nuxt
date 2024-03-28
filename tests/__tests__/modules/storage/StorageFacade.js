2eb1811a5dd3178a2eda9ed854a6998087d0fd18
Stew Eucen <stew.eucen@gmail.com>
2024-03-29 02:10:16 +0900

    Add test for StorageFacade.create()

--- tests/__tests__/modules/storage/StorageFacade.js
+++ tests/__tests__/modules/storage/StorageFacade.js
@@ -1,3 +1,7 @@
+import {
+  ConstructorSpyGenerator,
+} from '~/node_modules/@openreachtech/renchan-test-tools/index'
+
 import StorageFacade from '@/modules/storage/StorageFacade'
 
 describe('StorageFacade', () => {
@@ -27,6 +31,66 @@ describe('StorageFacade', () => {
           expect(facade)
             .toHaveProperty('storage', storageTally)
         })
+      })
+    })
+  })
+})
+
+describe('StorageFacade', () => {
+  describe('.create()', () => {
+    describe('to be instance of own class', () => {
+      const cases = [
+        {
+          params: {
+            storage: 'localStorage',
+          },
+        },
+        {
+          params: {
+            storage: 'sessionStorage',
+          },
+        },
+      ]
+
+      test.each(cases)('storage: $params.storage', ({ params }) => {
+        const storageTally = globalThis[params.storage]
+        const args = {
+          storage: storageTally,
+        }
+        const facade = StorageFacade.create(args)
+
+        expect(facade)
+          .toBeInstanceOf(StorageFacade)
+      })
+    })
+
+    describe('to call constructor', () => {
+      const cases = [
+        {
+          params: {
+            storage: 'localStorage',
+          },
+        },
+        {
+          params: {
+            storage: 'sessionStorage',
+          },
+        },
+      ]
+
+      test.each(cases)('storage: $params.storage', ({ params }) => {
+        const DerivedClass = ConstructorSpyGenerator.create({ jest })
+          .generateSpyKitClass(StorageFacade)
+
+        const storageTally = globalThis[params.storage]
+        const args = {
+          storage: storageTally,
+        }
+
+        DerivedClass.create(args)
+
+        expect(DerivedClass.__spy__)
+          .toHaveBeenCalledWith(args)
       })
     })
   })
