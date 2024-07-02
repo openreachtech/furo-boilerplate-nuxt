ae53489e3b84de04b212c17a53f7ad3216b865df
Stew Eucen <stew.eucen@gmail.com>
2024-07-03 02:55:51 +0900

    Rename filename of CurriculumsGraphqlCapsule to CurriculumsQueryGraphqlCapsule

--- app/graphql/client/Curriculums/CurriculumsGraphqlLauncher.js
+++ app/graphql/client/Curriculums/CurriculumsGraphqlLauncher.js
@@ -1,6 +1,6 @@
 import BaseAppGraphqlLauncher from '~/app/graphql/client/BaseAppGraphqlLauncher'
 import CurriculumsQueryGraphqlPayload from '~/app/graphql/client/Curriculums/CurriculumsQueryGraphqlPayload'
-import CurriculumsGraphqlCapsule from '~/app/graphql/client/Curriculums/CurriculumsGraphqlCapsule'
+import CurriculumsGraphqlCapsule from '~/app/graphql/client/Curriculums/CurriculumsQueryGraphqlCapsule'
 
 export default class CurriculumsGraphqlLauncher extends BaseAppGraphqlLauncher {
   /** @inheritdoc */
