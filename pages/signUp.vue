93293b2315bc9a10fd168b05290f49b5eafeb2fe
Stew Eucen <stew.eucen@gmail.com>
2024-07-04 05:06:55 +0900

    Build signUp page as stub

---
+++ pages/signUp.vue
@@ -0,0 +1,14 @@
+<!-- pages/signUp.vue -->
+<template>
+  <h1>Hello I&#39;m pages/signUp.vue!</h1>
+</template>
+
+<script>
+export default {
+  name: 'SignUpPage',
+}
+</script>
+
+<style>
+/* CSS styles here */
+</style>
