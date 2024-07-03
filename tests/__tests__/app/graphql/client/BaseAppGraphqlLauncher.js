68521ad5bd2b637db6a8548068fa719993ce3b64
Stew Eucen <stew.eucen@gmail.com>
2024-07-03 23:00:07 +0900

    Add test for BaseAppGraphqlLauncher#loadAccessToken()

--- tests/__tests__/app/graphql/client/BaseAppGraphqlLauncher.js
+++ tests/__tests__/app/graphql/client/BaseAppGraphqlLauncher.js
@@ -2,6 +2,10 @@ import BaseAppGraphqlLauncher from '@/app/graphql/client/BaseAppGraphqlLauncher'
 import BaseGraphqlLauncher from '~/modules/client/BaseGraphqlLauncher'
 import StorageFacade from '~/modules/storage/StorageFacade'
 
+beforeEach(() => {
+  localStorage.clear()
+})
+
 describe('BaseAppGraphqlLauncher', () => {
   describe('super class', () => {
     test('to be derived class of BaseGraphqlLauncher', () => {
@@ -105,6 +109,36 @@ describe('BaseAppGraphqlLauncher', () => {
           .toHaveBeenCalledWith()
 
         createAsLocalSpy.mockRestore()
+      })
+    })
+  })
+})
+
+describe('BaseAppGraphqlLauncher', () => {
+  describe('#loadAccessToken()', () => {
+    describe('with no params', () => {
+      const cases = [
+        {
+          args: {
+            accessToken: 'fc3ff98e8c6a0d308700000000000001',
+          },
+        },
+        {
+          args: {
+            accessToken: 'fc3ff98e8c6a0d308700000000000002',
+          },
+        },
+      ]
+
+      test.each(cases)('accessToken: $args.accessToken', ({ args }) => {
+        localStorage.setItem('access_token', args.accessToken)
+
+        const launcher = BaseAppGraphqlLauncher.create()
+
+        const actual = launcher.loadAccessToken()
+
+        expect(actual)
+          .toBe(args.accessToken)
       })
     })
   })
