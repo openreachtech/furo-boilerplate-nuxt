77c8e95071444f270b6a7e679934bbae22a2fb3a
Stew Eucen <stew.eucen@gmail.com>
2024-07-12 20:45:46 +0900

    Connect <form> to Ref

--- pages/samples/sign-up.vue
+++ pages/samples/sign-up.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>Hello I&#39;m pages/signUp.vue!</h1>
 
-  <form>
+  <form
+    ref="formRef"
+  >
     <label
       class="row"
       for="email"
@@ -79,8 +81,10 @@
 <script setup>
 import {
   reactive,
+  ref,
 } from 'vue'
 
+const formRef = ref(null)
 const statusReactive = reactive({
   allowsToSubmit: false,
 })
