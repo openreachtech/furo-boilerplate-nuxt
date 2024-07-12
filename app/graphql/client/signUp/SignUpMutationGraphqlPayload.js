af98e6df7dcf370663deb77bf9b73df50b1935d9
Stew Eucen <stew.eucen@gmail.com>
2024-07-13 02:09:07 +0900

    Define SignUpMutationGraphqlPayload.get:query

--- app/graphql/client/signUp/SignUpMutationGraphqlPayload.js
+++ app/graphql/client/signUp/SignUpMutationGraphqlPayload.js
@@ -1,5 +1,24 @@
 import BaseGraphqlPayload from '~/modules/client/BaseGraphqlPayload'
 
 export default class SignUpMutationGraphqlPayload extends BaseGraphqlPayload {
-
+  /** @inheritdoc */
+  static get query () {
+    return /* GraphQL */ `
+      mutation SignUpMutation ($input: SignUpInput!) {
+        signUp (input: $input) {
+          sentTo
+        }
+      }
+    `
+  }
 }
+
+/*
+ * SignUpInput {
+ *   email String!
+ *   username String
+ *   firstName String
+ *   lastName String
+ *   password String!
+ * }
+ */
