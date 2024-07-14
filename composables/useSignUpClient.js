11cbc13aecf443ca3265fb48a9b374384a082786
Stew Eucen <stew.eucen@gmail.com>
2024-07-14 14:01:13 +0900

    Define useSignUpClient() to call signUp GraphQL client

---
+++ composables/useSignUpClient.js
@@ -0,0 +1,35 @@
+import SignUpMutationGraphqlLauncher from '~/app/graphql/client/signUp/SignUpMutationGraphqlLauncher'
+
+export function useSignUpClient () {
+  return {
+    sendSignUp,
+  }
+
+  /**
+   * Fetch signUp.
+   *
+   * @param {{
+   *   variables: {
+   *     input: {
+   *       email: string
+   *       username?: string
+   *       firstName?: string
+   *       lastName?: string
+   *       password: string
+   *     }
+   *   }
+   * }} params - Parameters.
+   * @returns {Promise<import('~/app/graphql/client/signUp/SignUpMutationGraphqlCapsule')>}
+   */
+  async function sendSignUp ({
+    variables,
+  }) {
+    const launcher = SignUpMutationGraphqlLauncher.create()
+
+    const capsule = await launcher.launchQuery({
+      variables,
+    })
+
+    return capsule
+  }
+}
