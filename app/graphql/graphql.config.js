537ba3e88f3a5a86e4588737013a36cd84f06c18
Stew Eucen <stew.eucen@gmail.com>
2024-07-15 10:25:41 +0900

    Update default value of ENDPOINT_URL in graphql.config.js

--- app/graphql/graphql.config.js
+++ app/graphql/graphql.config.js
@@ -1,4 +1,4 @@
 export default {
-  ENDPOINT_URL: 'http://example.com/graphql-customer',
+  ENDPOINT_URL: 'http://localhost:3900/graphql-customer',
   // ENDPOINT_URL: 'https://clc-dev-front.openreach.tech/graphql-customer',
 }
