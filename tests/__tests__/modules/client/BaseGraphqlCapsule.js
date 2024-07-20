08307c315f7124be96cf6751b9bbd7ade449e5c5
Stew Eucen <stew.eucen@gmail.com>
2024-07-21 02:12:57 +0900

    Add test for BaseGraphqlCapsule.createAsNull()

--- tests/__tests__/modules/client/BaseGraphqlCapsule.js
+++ tests/__tests__/modules/client/BaseGraphqlCapsule.js
@@ -275,6 +275,36 @@ describe('BaseGraphqlCapsule', () => {
 })
 
 describe('BaseGraphqlCapsule', () => {
+  describe('.createAsNull()', () => {
+    describe('to be instance of own class', () => {
+      test('with no args', () => {
+        const actual = BaseGraphqlCapsule.createAsNull()
+
+        expect(actual)
+          .toBeInstanceOf(BaseGraphqlCapsule)
+      })
+    })
+
+    describe('to call .create()', () => {
+      test('with no args', () => {
+        const expectedArgs = {
+          rawResponse: null,
+          payload: null,
+          result: null,
+        }
+
+        const createSpy = jest.spyOn(BaseGraphqlCapsule, 'create')
+
+        BaseGraphqlCapsule.createAsNull()
+
+        expect(createSpy)
+          .toHaveBeenCalledWith(expectedArgs)
+      })
+    })
+  })
+})
+
+describe('BaseGraphqlCapsule', () => {
   describe('#isPending()', () => {
     const mockResponse = new Response()
     const mockResult = {
