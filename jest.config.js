f80942b1a86d39235162ac4ebeb3924287eb925a
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-20 18:22:56 +0700

    Create shared config object for Jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,27 @@
+const sharedConfig = {
+  collectCoverage: false,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'vue',
+    'json',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+  },
+  setupFiles: [
+    './jest/setup.js',
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': '@vue/vue3-jest',
+  },
+}
+
 export default {
   collectCoverage: false,
   collectCoverageFrom: [
