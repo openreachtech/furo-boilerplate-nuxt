75d8d0a557961e070e0ee3bcd753542f8127f2a6
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-20 18:27:57 +0700

    Add back "testEnvironmentOptions" for jest jsdom config

--- jest.config.js
+++ jest.config.js
@@ -29,6 +29,12 @@ export default {
 
       displayName: 'jsdom',
       testEnvironment: 'jsdom',
+      testEnvironmentOptions: {
+        customExportConditions: [
+          'node',
+          'node-addons',
+        ],
+      },
       testMatch: [ // '**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)',
         '**/__tests__/jsdom/**/*.[jt]s?(x)',
         '**/jsdom/?(*.)+(spec|test).[jt]s?(x)',
