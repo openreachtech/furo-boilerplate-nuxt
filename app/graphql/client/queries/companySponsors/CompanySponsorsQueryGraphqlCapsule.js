b355ab508ddc175df92bfe61d2a023e0e6efbea3
Stew Eucen <stew.eucen@gmail.com>
2024-07-26 07:26:35 +0900

    Add companySponsors GraphQL client classes

---
+++ app/graphql/client/queries/companySponsors/CompanySponsorsQueryGraphqlCapsule.js
@@ -0,0 +1,60 @@
+import BaseGraphqlCapsule from '~/modules/client/BaseGraphqlCapsule'
+
+export default class CompanySponsorsQueryGraphqlCapsule extends BaseGraphqlCapsule {
+  /**
+   * Factory method to create as pending behavior.
+   *
+   * @override
+   * @returns {CompanySponsorsQueryGraphqlCapsule} Instance of this class.
+   */
+  static createAsPending () {
+    return /** @type {*} */ (
+      super.createAsPending()
+    )
+  }
+
+  /**
+   * Extract content from response.
+   *
+   * @override
+   * @returns {CompanySponsorsContent | null} Content
+   */
+  extractContent () {
+    return /** @type {*} */ (
+      super.extractContent()
+    )
+  }
+
+  /**
+   * get: companySponsors
+   *
+   * @returns {Array<CompanySponsorEntity>} Array of company sponsor.
+   */
+  get companySponsors () {
+    const content = this.extractContent()
+
+    return content
+      ?.companySponsors
+      ?.companySponsors
+      ?? []
+  }
+}
+
+/**
+ * @typedef {{
+ *   companySponsors: {
+ *     companySponsors: Array<CompanySponsorEntity>
+ *   }
+ * }} CompanySponsorsContent
+ */
+
+/**
+ * @typedef {{
+ *   id: number
+ *   registeredAt: string
+ *   companyName: string
+ *   companyDescription: string
+ *   companySponsorHomepage: string
+ *   companySponsorLogo: string
+ * }} CompanySponsorEntity
+ */
