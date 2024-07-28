4a5766ef319444a0e2c924fbd6e7eca6dcce4830
Stew Eucen <stew.eucen@gmail.com>
2024-07-28 23:32:13 +0900

    Declare FormControlElementClerk as stub

---
+++ modules/domClerks/FormControlElementClerk.js
@@ -0,0 +1,45 @@
+export default class FormControlElementClerk {
+  /**
+   * Constructor of FormControlElementClerk.
+   *
+   * @param {FormControlElementClerkParams} params
+   */
+  constructor ({
+    control,
+  }) {
+    this.control = control
+  }
+
+  /**
+   * Factory method of FormControlElementClerk.
+   *
+   * @param {FormControlElementClerkFactoryParams} params
+   * @returns
+   */
+  static create ({
+    control,
+  }) {
+    return new this({
+      control,
+    })
+  }
+}
+
+/**
+ * @typedef {{
+ *   control: FormControlElementType
+ * }} FormControlElementClerkParams
+ */
+
+/**
+ * @typedef {FormControlElementClerkParams} FormControlElementClerkFactoryParams
+ */
+
+/**
+ * @typedef {HTMLButtonElement
+ *   | HTMLInputElement
+ *   | HTMLSelectElement
+ *   | HTMLTextAreaElement
+ *   | RadioNodeList
+ * } FormControlElementType
+ */
