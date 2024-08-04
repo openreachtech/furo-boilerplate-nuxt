d64f084942c229d328cd2ccf7495c74539eedacd
Stew Eucen <stew.eucen@gmail.com>
2024-08-05 08:57:28 +0900

    Define response type of SignUpMutationGraphqlCapsule

--- app/graphql/client/signUp/SignUpMutationGraphqlCapsule.js
+++ app/graphql/client/signUp/SignUpMutationGraphqlCapsule.js
@@ -3,3 +3,9 @@ import BaseGraphqlCapsule from '~/modules/client/BaseGraphqlCapsule'
 export default class SignUpMutationGraphqlCapsule extends BaseGraphqlCapsule {
 
 }
+
+/**
+ * @typedef {{
+ *   sentTo: string
+ * }} SignUpContent
+ */
