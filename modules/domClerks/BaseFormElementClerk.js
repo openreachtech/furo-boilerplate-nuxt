a228e19d410bf2bb4e629fc3aff474aeba90166d
Stew Eucen <stew.eucen@gmail.com>
2024-08-07 17:37:37 +0900

    Add generics type of BaseFormElementClerk

--- modules/domClerks/BaseFormElementClerk.js
+++ modules/domClerks/BaseFormElementClerk.js
@@ -6,6 +6,8 @@ import FieldValidator from '~/modules/client/FieldValidator'
  * Base class of form element clerk.
  *
  * @template T
+ * @template {Record<string, string | Array<string> | null>} FV
+ * @template {Record<string, *>} SV
  */
 export default class BaseFormElementClerk {
   /**
@@ -23,7 +25,7 @@ export default class BaseFormElementClerk {
    * Factory method of BaseFormElementClerk.
    *
    * @param {BaseFormElementClerkFactoryParams} params - Parameters of factory method.
-   * @template {typeof BaseFormElementClerk} T
+   * @template {typeof BaseFormElementClerk<*, *, *>} T
    * @this {T}
    * @returns {InstanceType<T>} Instance of this class.
    */
