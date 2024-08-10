d4d74a4c2e7c8559341f249c25c2bf5b40d4bd6f
Stew Eucen <stew.eucen@gmail.com>
2024-08-11 07:25:33 +0900

    Add test for BaseAppGraphqlPayload.createStorageClerk()

--- tests/__tests__/app/graphql/client/BaseAppGraphqlPayload.js
+++ tests/__tests__/app/graphql/client/BaseAppGraphqlPayload.js
@@ -1,5 +1,6 @@
 import BaseAppGraphqlPayload from '~/app/graphql/client/BaseAppGraphqlPayload'
 import BaseGraphqlPayload from '~/modules/client/BaseGraphqlPayload'
+import StorageClerk from '~/modules/storage/StorageClerk'
 
 beforeEach(() => {
   localStorage.clear()
@@ -12,6 +13,38 @@ describe('BaseAppGraphqlPayload', () => {
 
       expect(actual)
         .toBeInstanceOf(BaseGraphqlPayload)
+    })
+  })
+})
+
+describe('BaseAppGraphqlPayload', () => {
+  describe('.createStorageClerk()', () => {
+    describe('to return instance of StorageClerk', () => {
+      test('with no params', () => {
+        const storageClerk = BaseAppGraphqlPayload.createStorageClerk()
+
+        expect(storageClerk)
+          .toBeInstanceOf(StorageClerk)
+      })
+    })
+
+    describe('to call StorageClerk.createAsLocal()', () => {
+      test('with no params', () => {
+        const storageClerkTally = /** @type {StorageClerk} */ ({})
+
+        const createAsLocalSpy = jest.spyOn(StorageClerk, 'createAsLocal')
+          .mockReturnValue(storageClerkTally)
+
+        const actual = BaseAppGraphqlPayload.createStorageClerk()
+
+        expect(actual)
+          .toBe(storageClerkTally) // same reference
+
+        expect(createAsLocalSpy)
+          .toHaveBeenCalledWith()
+
+        createAsLocalSpy.mockRestore()
+      })
     })
   })
 })
