090a8eabc1e717e6dc86126c74d51cd449e57351
Stew Eucen <stew.eucen@gmail.com>
2024-08-11 21:27:50 +0900

    Fix <input> name of password-confirmation

--- pages/samples/sign-up.vue
+++ pages/samples/sign-up.vue
@@ -122,7 +122,7 @@ async function submitForm ({
     <label class="row">
       <span>パスワード (確認用)</span>
       <input
-        name="confirm-password"
+        name="password-confirmation"
         type="password"
         placeholder="パスワードを入力してください。"
       >
