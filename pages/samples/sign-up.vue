553cfe28fa2dd59b3a421965040e145b54129aa6
Stew Eucen <stew.eucen@gmail.com>
2024-08-20 12:40:36 +0900

    Replace useFormClerk() with furo in sign-up page

--- pages/samples/sign-up.vue
+++ pages/samples/sign-up.vue
@@ -4,12 +4,14 @@ import {
   ref,
 } from 'vue'
 
+import {
+  useFormClerk,
+} from '@openreachtech/furo-nuxt'
+
 import useGraphqlClient from '~/modules/composables/useGraphqlClient'
 import SignUpMutationGraphqlLauncher from '~/app/graphql/client/mutations/signUp/SignUpMutationGraphqlLauncher'
 
 import SignUpFormElementClerk from '~/app/domClerk/SignUpFormElementClerk'
-
-import useFormClerk from '~/modules/composables/useFormClerk'
 
 const formRef = ref(null)
 const statusReactive = reactive({
