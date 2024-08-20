30ffaf85b8788844b5037677875b294ad8e0e2c2
Stew Eucen <stew.eucen@gmail.com>
2024-08-20 19:08:16 +0900

    Update test for SignUpMutationGraphqlCapsule

--- tests/__tests__/app/graphql/client/mutations/signUp/SignUpMutationGraphqlCapsule.js
+++ tests/__tests__/app/graphql/client/mutations/signUp/SignUpMutationGraphqlCapsule.js
@@ -1,16 +1,14 @@
-import {
-  BaseGraphqlCapsule,
-} from '@openreachtech/furo'
+import BaseAppGraphqlCapsule from '~/app/graphql/client/BaseAppGraphqlCapsule'
 
 import SignUpMutationGraphqlCapsule from '~/app/graphql/client/mutations/signUp/SignUpMutationGraphqlCapsule'
 
 describe('SignUpMutationGraphqlCapsule', () => {
   describe('super class', () => {
-    test('to be derived class of BaseGraphqlCapsule', () => {
+    test('to be derived class of BaseAppGraphqlCapsule', () => {
       const actual = SignUpMutationGraphqlCapsule.prototype
 
       expect(actual)
-        .toBeInstanceOf(BaseGraphqlCapsule)
+        .toBeInstanceOf(BaseAppGraphqlCapsule)
     })
   })
 })
