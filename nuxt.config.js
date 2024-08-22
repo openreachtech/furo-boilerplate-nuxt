5b4125944f0f7fafe531a212fb484711e3ecbdb6
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-08-22 12:32:34 +0700

    Correct `head` setting position

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,20 +4,22 @@ import {
 
 // Reference: https://nuxt.com/docs/api/nuxt-config.
 export default defineNuxtConfig({
-  head: {
-    title: 'furo-boilerplate',
-    htmlAttrs: {
-      lang: 'en',
+  app: {
+    head: {
+      title: 'furo-boilerplate',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    ],
   },
 
   css: [
