982abaa3940ff337d101a91c029c22d4e55177d5
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2024-09-14 14:34:33 +0700

    Add `vue/no-multiple-objects-in-class` rule

--- eslint.config.js
+++ eslint.config.js
@@ -281,6 +281,9 @@ export default [
       'vue/no-duplicate-attr-inheritance': [
         'error',
       ],
+      'vue/no-multiple-objects-in-class': [
+        'error',
+      ],
       'vue/padding-line-between-blocks': [
         'error',
         'always',
