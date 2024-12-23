3f48e99a68a1604767a0cb4f614bfbb0e83943b8
Stew Eucen <stew.eucen@gmail.com>
2024-12-23 22:24:10 +0900

    Build AppPatination component

---
+++ components/units/AppPagination.vue
@@ -0,0 +1,55 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import FuroPagination from '~/components/lib/FuroPagination.vue'
+
+export default defineComponent({
+  name: 'AppPagination',
+
+  components: {
+    FuroPagination,
+  },
+
+  inheritAttrs: false,
+
+  setup (props) {
+    return {
+      props,
+    }
+  },
+})
+</script>
+
+<template>
+  <FuroPagination class="design"
+    v-bind="$attrs"
+  >
+    <template #previous>
+      <slot name="previous">
+        ←
+      </slot>
+    </template>
+    <template #next>
+      <slot name="next">
+        →
+      </slot>
+    </template>
+  </FuroPagination>
+</template>
+
+<!-- can not use scoped here -->
+<style>
+/*
+ * Can customize the pagination component.
+ */
+.unit-pagination.design {
+  gap: 0.5rem;
+}
+
+.unit-pagination.design > .page {
+  border: 3px double var(--color-primary);
+  border-radius: 0.25rem;
+}
+</style>
