92aff43f0ff4a8734d3a3cbb87db3c231f9b9108
Stew Eucen <stew.eucen@gmail.com>
2024-12-27 14:22:59 +0900

    Build FuroTabLayout component

---
+++ components/lib/FuroTabLayout.vue
@@ -0,0 +1,134 @@
+<script>
+import {
+  defineComponent,
+  ref,
+} from 'vue'
+
+import FuroTabLayoutContext from '~/app/vue/contexts/FuroTabLayoutContext.js'
+
+export default defineComponent({
+  name: 'FuroTabLayout',
+  inheritAttrs: false,
+
+  props: {
+    tabs: {
+      type: Array,
+      default: () => [],
+      validator: value => Array.isArray(value),
+    },
+    activeTabKey: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+
+  /**
+   * Setup the component.
+   *
+   * @param {{
+   *   tabs: Array<* &
+   *     import('~/app/vue/contexts/FuroTabLayoutContext.js').FuroTabParams
+   *   >
+   *   activeTabKey: string
+   * }} props
+   * @returns {{
+   *   context: FuroTabLayoutContext
+   *   tabsRef: import('vue').Ref<Array<HTMLElement>>
+   * }}
+   */
+  setup (props) {
+    const tabsRef = ref([])
+
+    const context = FuroTabLayoutContext.create({
+      props,
+    })
+
+    return {
+      context,
+      tabsRef,
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="unit-tablayout"
+    v-bind="$attrs"
+  >
+    <div class="tabs">
+      <button v-for="(tab, index) in context.tabContexts"
+        :key="index"
+        ref="tabsRef"
+        class="tab"
+        :class="{
+          active: tab.isTargetTab({
+            tabKey: activeTabKey,
+          }),
+        }"
+        @click="context.onClickTab({
+          event: $event,
+          tabsRef,
+        })"
+      >
+        {{ tab.label }}
+      </button>
+    </div>
+
+    <div class="contents">
+      <slot name="contents" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* tabs */
+
+.unit-tablayout > .tabs {
+  display: flex;
+  flex-direction: row;
+}
+
+.unit-tablayout > .tabs > .tab {
+  border: none;
+  appearance: none;
+
+  padding-block: 0.25rem;
+  padding-inline: 0.5rem;
+}
+
+.unit-tablayout > .tabs > .tab:hover {
+  background-color: var(--color-background-highlight);
+  color: var(--color-text-highlight);
+
+  cursor: pointer;
+}
+
+.unit-tablayout > .tabs > .tab.active {
+  background-color: var(--color-background-highlight);
+  color: var(--color-text-highlight);
+
+  pointer-events: none;
+}
+
+/* tab gimmick */
+
+.unit-tablayout > .contents > * {
+  display: none;
+}
+
+.unit-tablayout:has(.tab:nth-of-type(1).active) > .contents > :nth-of-type(1),
+.unit-tablayout:has(.tab:nth-of-type(2).active) > .contents > :nth-of-type(2),
+.unit-tablayout:has(.tab:nth-of-type(3).active) > .contents > :nth-of-type(3),
+.unit-tablayout:has(.tab:nth-of-type(4).active) > .contents > :nth-of-type(4),
+.unit-tablayout:has(.tab:nth-of-type(5).active) > .contents > :nth-of-type(5),
+.unit-tablayout:has(.tab:nth-of-type(6).active) > .contents > :nth-of-type(6),
+.unit-tablayout:has(.tab:nth-of-type(7).active) > .contents > :nth-of-type(7),
+.unit-tablayout:has(.tab:nth-of-type(8).active) > .contents > :nth-of-type(8),
+.unit-tablayout:has(.tab:nth-of-type(9).active) > .contents > :nth-of-type(9),
+.unit-tablayout:has(.tab:nth-of-type(10).active) > .contents > :nth-of-type(10),
+.unit-tablayout:has(.tab:nth-of-type(11).active) > .contents > :nth-of-type(11),
+.unit-tablayout:has(.tab:nth-of-type(12).active) > .contents > :nth-of-type(12) {
+  display: inherit;
+}
+</style>
