9ec28fca874a9b676effd7ab9bf94c05d3c4c3ed
Stew Eucen <stew.eucen@gmail.com>
2025-01-05 00:48:28 +0900

    Update test for FuroButtonDialogContext.create()

--- tests/__tests__/jsdom/app/vue/contexts/FuroButtonDialogContext.js
+++ tests/__tests__/jsdom/app/vue/contexts/FuroButtonDialogContext.js
@@ -84,35 +84,43 @@ describe('FuroButtonDialogContext', () => {
 
 describe('FuroButtonDialogContext', () => {
   describe('.create()', () => {
+    const propsMock = {}
+    const componentContextMock = {
+      attrs: {},
+      emit: () => {},
+      expose: () => {},
+      slots: {},
+    }
+
     /**
      * @type {Array<{
      *   params: {
+     *     props: import('vue').ComponentCustomProps
+     *     componentContext: import('vue').SetupContext
      *     dialogComponentRef: import('vue').Ref<import('~/components/lib/FuroDialog.vue').default | null>
-     *     emit: (
-     *       event: 'clickBackdrop' | 'clickPositiveButton' | 'clickNegativeButton' | 'clickNeutralButton',
-     *       ...args: Array<any>
-     *     ) => void
      *   }
      * }>}
      */
     const cases = /** @type {Array<*>} */ ([
       {
         params: {
+          props: propsMock,
+          componentContext: componentContextMock,
           dialogComponentRef: {
             value: {
               showDialog: () => {},
               dismissDialog: () => {},
             },
           },
-          emit: () => {},
         },
       },
       {
         params: {
+          props: propsMock,
+          componentContext: componentContextMock,
           dialogComponentRef: {
             value: null,
           },
-          emit: () => {},
         },
       },
     ])
