f71e329302d39811a6610dde9a75561c3ecb8e70
Stew Eucen <stew.eucen@gmail.com>
2025-02-05 03:19:11 +0900

    Retake furo-dialog sample page with furo-nuxt

---
+++ pages/samples/components/furo-dialog/FormDialogPageContext.js
@@ -0,0 +1,124 @@
+import {
+  BaseFuroContext,
+} from '@openreachtech/furo-nuxt'
+
+/**
+ * FormDialogPageContext.
+ *
+ * @extends {BaseFuroContext<null>} - Base class.
+ */
+export default class FormDialogPageContext extends BaseFuroContext {
+  /**
+   * Constructor.
+   *
+   * @param {FormDialogPageContextParams} params - Parameters of this constructor.
+   */
+  constructor ({
+    props,
+    componentContext,
+
+    formDialogRef,
+    formElementRef,
+    formValuesRef,
+  }) {
+    super({
+      props,
+      componentContext,
+    })
+
+    this.formDialogRef = formDialogRef
+    this.formElementRef = formElementRef
+    this.formValuesRef = formValuesRef
+  }
+
+  /**
+   * Factory method to create a new instance of this class.
+   *
+   * @template {X extends typeof FormDialogPageContext ? X : never} T, X
+   * @override
+   * @param {FormDialogPageContextFactoryParams} params - Parameters of this factory method.
+   * @returns {InstanceType<T>} - An instance of this class.
+   * @this {T}
+   */
+  static create ({
+    props,
+    componentContext,
+
+    formDialogRef,
+    formElementRef,
+    formValuesRef,
+  }) {
+    return /** @type {InstanceType<T>} */ (
+      new this({
+        props,
+        componentContext,
+
+        formDialogRef,
+        formElementRef,
+        formValuesRef,
+      })
+    )
+  }
+
+  /**
+   * get: form values
+   *
+   * @returns {string} - response text.
+   */
+  get formValues () {
+    return this.formValuesRef.value
+  }
+
+  /**
+   * Show dialog.
+   */
+  showDialog () {
+    this.formDialogRef.value
+      ?.showDialog()
+  }
+
+  /**
+   * Dismiss dialog.
+   */
+  dismissDialog () {
+    this.formDialogRef.value
+      ?.dismissDialog()
+  }
+
+  /**
+   * Submit form.
+   *
+   * @param {{
+   *   formElement: HTMLFormElement
+   * }} params - Parameters.
+   */
+  submitForm ({
+    formElement,
+  }) {
+    const value = formElement
+      ? Object.fromEntries(
+        new FormData(formElement) // eslint-disable-line
+      )
+      : null
+
+    this.formValuesRef.value = JSON.stringify(
+      value,
+      null,
+      2
+    )
+
+    this.dismissDialog()
+  }
+}
+
+/**
+ * @typedef {import('@openreachtech/furo-nuxt/lib/contexts/BaseFuroContext.js').BaseFuroContextParams & {
+ *   formDialogRef: import('vue').Ref<import('@openreachtech/furo-nuxt/lib/components/FuroDialog.vue').default | null>
+ *   formElementRef: import('vue').Ref<HTMLFormElement | null>
+ *   formValuesRef: import('vue').Ref<string>
+ * }} FormDialogPageContextParams
+ */
+
+/**
+ * @typedef {FormDialogPageContextParams} FormDialogPageContextFactoryParams
+ */
