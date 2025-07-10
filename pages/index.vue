61b51856aaab0b5a35a81b83e958ca7950c29b6f
Stew Eucen <stew.eucen@gmail.com>
2025-07-10 16:15:30 +0900

    Retake index.vue to kick out links

--- pages/index.vue
+++ pages/index.vue
@@ -5,99 +5,98 @@ import {
 } from 'vue'
 
 import {
-  NuxtLink,
-} from '#components'
+  definePageMeta,
+} from '#imports'
 
 export default defineComponent({
   name: 'IndexPage',
 
-  components: {
-    NuxtLink,
+  setup () {
+    definePageMeta({
+      $furo: {
+        pageTitle: 'Home',
+      },
+    })
+
+    return {}
   },
 })
 </script>
 
 <template>
-  <h1>Hello I&#39;m pages/index.vue!</h1>
-  <div>
-    <NuxtLink to="samples/curriculums">
-      Curriculums
-    </NuxtLink>
-  </div>
-  <div>
-    <NuxtLink to="samples/company-sponsors">
-      Company Sponsors
-    </NuxtLink>
-  </div>
-  <div>
-    <NuxtLink to="samples/sign-up">
-      Sign Up
-    </NuxtLink>
-  </div>
-  <div>
-    <NuxtLink to="samples/sign-in">
-      Sign In
-    </NuxtLink>
-  </div>
-  <div>
-    <NuxtLink to="samples/upload-image">
-      Upload Image
-    </NuxtLink>
-  </div>
-  <div>
-    <NuxtLink to="samples/pagination-articles">
-      Pagination Articles
-    </NuxtLink>
-  </div>
-  <br>
-  <div>
-    <NuxtLink to="samples/form-controls-inspector">
-      Form Controls Inspector
-    </NuxtLink>
-  </div>
-  <div>
-    <NuxtLink to="samples/trigger-unlock">
-      Trigger Unlock Sample
-    </NuxtLink>
-  </div>
-  <br>
-  <div>
-    <NuxtLink to="samples/components/furo-pagination">
-      Furo Pagination Sample
-    </NuxtLink>
-  </div>
-  <div>
-    <NuxtLink to="samples/components/furo-tab-layout">
-      Furo TabLayout Sample
-    </NuxtLink>
-  </div>
-  <div>
-    <NuxtLink to="samples/components/furo-dialog">
-      Furo Dialog Sample
-    </NuxtLink>
-  </div>
-  <div>
-    <NuxtLink to="samples/components/furo-loading-layout">
-      Furo LoadingLayout Sample
-    </NuxtLink>
-  </div>
-  <br>
-  <div>
-    <NuxtLink to="samples/context/using-computed">
-      computed() via Context
-    </NuxtLink>
-  </div>
-  <br>
-  <div>
-    <NuxtLink to="samples/chat-room?room=1">
-      Chat Room [1]
-    </NuxtLink>
-  </div>
-  <div>
-    <NuxtLink to="samples/chat-room?room=2">
-      Chat Room [2]
-    </NuxtLink>
-  </div>
+  <h1>Hello I&#39;m index.vue!</h1>
+
+  <section>
+    <button onclick="alert('Hello from the button!')">
+      Click me to confirm the event works!
+    </button>
+  </section>
+
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
 </template>
 
 <style scoped>
