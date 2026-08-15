328889d956c8e9c29b7928ba04342aeacd65a9e9
hieuguyen-ort <hieu.nguyen.202510@openreach.tech>
2026-08-16 00:34:22 +0700

    Update test for BaseAppGraphqlPayload

--- tests/__tests__/jsdom/app/graphql/client/BaseAppGraphqlPayload.js
+++ tests/__tests__/jsdom/app/graphql/client/BaseAppGraphqlPayload.js
@@ -1,13 +1,8 @@
 import {
   BaseGraphqlPayload,
-  StorageClerk,
 } from '@openreachtech/furo'
 
 import BaseAppGraphqlPayload from '~/app/graphql/client/BaseAppGraphqlPayload'
-
-beforeEach(() => {
-  localStorage.clear()
-})
 
 describe('BaseAppGraphqlPayload', () => {
   describe('super class', () => {
@@ -16,118 +11,6 @@ describe('BaseAppGraphqlPayload', () => {
 
       expect(actual)
         .toBeInstanceOf(BaseGraphqlPayload)
-    })
-  })
-})
-
-describe('BaseAppGraphqlPayload', () => {
-  describe('.createStorageClerk()', () => {
-    describe('to return instance of StorageClerk', () => {
-      test('with no params', () => {
-        const storageClerk = BaseAppGraphqlPayload.createStorageClerk()
-
-        expect(storageClerk)
-          .toBeInstanceOf(StorageClerk)
-      })
-    })
-
-    describe('to call StorageClerk.createAsLocal()', () => {
-      test('with no params', () => {
-        const storageClerkTally = /** @type {StorageClerk} */ ({})
-
-        const createAsLocalSpy = jest.spyOn(StorageClerk, 'createAsLocal')
-          .mockReturnValue(storageClerkTally)
-
-        const actual = BaseAppGraphqlPayload.createStorageClerk()
-
-        expect(actual)
-          .toBe(storageClerkTally) // same reference
-
-        expect(createAsLocalSpy)
-          .toHaveBeenCalledWith()
-
-        createAsLocalSpy.mockRestore()
-      })
-    })
-  })
-})
-
-describe('BaseAppGraphqlPayload', () => {
-  describe('.loadAccessToken()', () => {
-    describe('with no params', () => {
-      const cases = [
-        {
-          args: {
-            accessToken: 'fc3ff98e8c6a0d308700000000000001',
-          },
-        },
-        {
-          args: {
-            accessToken: 'fc3ff98e8c6a0d308700000000000002',
-          },
-        },
-      ]
-
-      test.each(cases)('accessToken: $args.accessToken', ({ args }) => {
-        localStorage.setItem('access_token', args.accessToken)
-
-        const actual = BaseAppGraphqlPayload.loadAccessToken()
-
-        expect(actual)
-          .toBe(args.accessToken)
-      })
-    })
-  })
-})
-
-describe('BaseAppGraphqlPayload', () => {
-  describe('.collectBasedHeadersOptions()', () => {
-    describe('to add `x-renchan-access-token`', () => {
-      const cases = [
-        {
-          args: {
-            accessToken: 'fc3ff98e8c6a0d308700000000000001',
-          },
-          expected: [
-            {
-              'x-renchan-access-token': 'fc3ff98e8c6a0d308700000000000001',
-            },
-          ],
-        },
-        {
-          args: {
-            accessToken: 'fc3ff98e8c6a0d308700000000000002',
-          },
-          expected: [
-            {
-              'x-renchan-access-token': 'fc3ff98e8c6a0d308700000000000002',
-            },
-          ],
-        },
-      ]
-
-      test.each(cases)('accessToken: $args.accessToken', ({ args, expected }) => {
-        localStorage.setItem('access_token', args.accessToken)
-
-        const actual = BaseAppGraphqlPayload.collectBasedHeadersOptions()
-
-        expect(actual)
-          .toEqual(expected)
-      })
-    })
-
-    describe('to not add `x-renchan-access-token`', () => {
-      test('with no args', () => {
-        localStorage.removeItem('access_token')
-
-        /** @type {Array<Record<string, string>>} */
-        const expected = []
-
-        const actual = BaseAppGraphqlPayload.collectBasedHeadersOptions()
-
-        expect(actual)
-          .toEqual(expected)
-      })
     })
   })
 })
