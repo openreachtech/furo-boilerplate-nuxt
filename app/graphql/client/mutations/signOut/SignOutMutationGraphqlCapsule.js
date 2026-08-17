a373c2bb1f7df65e4a64cb9ebdde2bbd96fb4dda
hieuguyen-ort <hieu.nguyen.202510@openreach.tech>
2026-08-17 17:01:13 +0700

    Add signOut mutation graphql client

---
+++ app/graphql/client/mutations/signOut/SignOutMutationGraphqlCapsule.js
@@ -0,0 +1,30 @@
+import BaseAppGraphqlCapsule from '~/app/graphql/client/BaseAppGraphqlCapsule'
+
+/**
+ * SignOut mutation graphql capsule.
+ *
+ * @extends {BaseAppGraphqlCapsule<SignOutMutationResponseContent>}
+ */
+export default class SignOutMutationGraphqlCapsule extends BaseAppGraphqlCapsule {
+  /**
+   * get: isSignedOut
+   *
+   * @returns {boolean | null} Whether the session was signed out.
+   */
+  get isSignedOut () {
+    const content = this.extractContent()
+
+    return content
+      ?.signOut
+      ?.isSignedOut
+      ?? null
+  }
+}
+
+/**
+ * @typedef {{
+ *   signOut: {
+ *     isSignedOut: boolean
+ *   }
+ * }} SignOutMutationResponseContent
+ */
